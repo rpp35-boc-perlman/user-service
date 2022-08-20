276383e501b6cf359387a9a0c6cb167743eab289
Chris Sampson <csamposn@gmail.com>
2022-08-20 17:34:53 -0400

    Added 404 handler

--- server.js
+++ server.js
@@ -4,18 +4,25 @@ const app = express()
 
 app.use(express.json())
 
-app.get('/heartbeat', (req,res,next) => {
+app.get('/api/heartbeat', (req,res,next) => {
     res.status(200).send('up');
 })
 
 // routers
 app.use('/api/users', require('./routes/users'));
 
+
+// 404 handler
+app.use((req,res) => {
+    res.status(404).send('Not Found')
+})
+
 // catch all error handler
 app.use((err, req, res, next) => {
+    console.log('Uncaught Error'.red)
     res.status(500).json({
         status: 500,
-        message: "uncaught error",
+        message: "Uncaught error",
         error: err.message
     })
 })
--- tests/server.test.js
+++ tests/server.test.js
@@ -4,14 +4,32 @@ const express = require('express');
 const app = require('../server.js')
 
 describe('Server Heartbeat Test', () => {
+    let server = null
+
+    beforeEach(() => {
+        server = app.listen(3300)
+    })
+
+    afterEach(() => {
+        server.close()
+    })
 
     it('Should have a heartbeat', async () => {
         try{
-            const r = await request(app).get('http://localhost:3300/heartbeat')
+            const r = await request(server).get('http://localhost:3300/api/heartbeat')
             expect(r.status).toEqual(200)
         } catch (err) {
 
         }
     }, 5000);
+
+    it('Should have a 404 handler' , async () => {
+        try{
+            const r = await request(server).get('http://localhost:3300/nothere')
+            expect(r.status).toEqual(404)
+        } catch (err) {
+
+        }
+    }, 5000)
 
 })
